7bddb11038409b357204b98e21efa2cab8707c9d
bashar-karoury <basharalkaroury@hotmail.com>
2024-09-05 11:02:31 +0300

    Add Home Page

---
+++ auth.js
@@ -0,0 +1,13 @@
+document.querySelector('#login').addEventListener('click', function () {
+	console.log("login is clicked");
+	// hide login and register buttons
+	// show login panel
+	// add listener to login button
+});
+
+document.querySelector('#register').addEventListener('click', function () {
+	console.log("register is clicked");
+	// hide login and register buttons
+	// show register panel
+	// add listener to register button
+});
