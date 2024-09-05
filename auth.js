a14c89322b920aa69f0f20c715aff3b69eafb4ae
bashar-karoury <basharalkaroury@hotmail.com>
2024-09-05 13:24:58 +0300

    Add TODO register_now and login result

--- auth.js
+++ auth.js
@@ -1,13 +1,37 @@
 document.querySelector('#login').addEventListener('click', function () {
 	console.log("login is clicked");
-	// hide login and register buttons
+	// hide athentication buttons
+	document.querySelector('.buttons_container').classList.toggle('hidden');
 	// show login panel
+	document.querySelector('.login_container').classList.toggle('hidden');
 	// add listener to login button
+	document.querySelector('.login_container .submit').addEventListener('click', function () {
+		console.log("login submit is clicked");
+		// sanitize inputs and add alert user if one of them is not accepted// todo
+		// send POST request to backend
+		// alert the user of result of login
+	});
+	/* add event listener to click on register_now that gets you to rigester form */
+
 });
 
 document.querySelector('#register').addEventListener('click', function () {
 	console.log("register is clicked");
-	// hide login and register buttons
-	// show register panel
-	// add listener to register button
+	// hide athentication buttons
+	document.querySelector('.buttons_container').classList.toggle('hidden');
+	// show login panel
+	document.querySelector('.register_container').classList.toggle('hidden');
+	// add listener to login button
+	document.querySelector('.register_container .submit').addEventListener('click', function () {
+		console.log("register submit is clicked");
+		// sanitize inputs // todo
+		// send POST request to backend
+		// alert result pop up ?
+		// show login container
+		document.querySelector('.register_container').classList.toggle('hidden');
+		document.querySelector('.login_container').classList.toggle('hidden');
+
+	});
 });
+
+
